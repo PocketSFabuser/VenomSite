6b11d5be3a12d40673d5e52b3b5984b0880845f8
PocketSFabuser <kolos.comm2@gmail.com>
2025-03-01 17:43:53 +0300

    font-size fix

--- pass.js
+++ pass.js
@@ -1,4 +1,3 @@
-// JavaScript code to apply styles based on media queries
 if (window.matchMedia("(max-width: 750px)").matches) {
     // Load mobile styles
     document.write('<link rel="stylesheet" type="text/css" href="m-stylefile.css">');
@@ -6,14 +5,3 @@ if (window.matchMedia("(max-width: 750px)").matches) {
     // Load desktop styles
     document.write('<link rel="stylesheet" type="text/css" href="stylefile.css">');
 }
-
-// Add an event listener to handle window resizing
-window.addEventListener('resize', function() {
-    if (window.matchMedia("(max-width: 750px)").matches) {
-        // Load mobile styles
-        document.write('<link rel="stylesheet" type="text/css" href="m-stylefile.css">');
-    } else {
-        // Load desktop styles
-        document.write('<link rel="stylesheet" type="text/css" href="stylefile.css">');
-    }
-});
